49d6fc223f4f82c90646d1dc4e1058e193238c27
MB <mb150996@gmail.com>
2021-05-31 04:46:07 +0300

    alioth: Build dtb and dtbo
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,3 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/alioth
 TARGET_KERNEL_CONFIG := vendor/syberia_defconfig
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
